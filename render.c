46d2ef75ac63000542ab228b187285b609c9bda8
Buket Çili <bcili@k1m34s03.42istanbul.com.tr>
2025-04-07 13:40:12 +0300

    fract-ol_42

--- render.c
+++ render.c
@@ -24,8 +24,8 @@ static int	get_color(int i, int max_iterations, char *fractal_name)
 {
 	double	t;
 	int		red;
-	int     green;
-	int     blue;
+	int		green;
+	int		blue;
 
 	t = (double)i / max_iterations;
 	if (!ft_strcmp(fractal_name, "mandelbrot"))
@@ -34,7 +34,7 @@ static int	get_color(int i, int max_iterations, char *fractal_name)
 		green = 255 - (int)(18 * (1 - t) * (1 - t) * t * t * 180);
 		blue = 255 - (int)(9 * (1 - t) * (1 - t) * (1 - t) * t * 220);
 	}
-	else if (!ft_strcmp(fractal_name, "julia"))
+	else
 	{
 		red = 255 - (int)(16 * (1 - t) * t * t * 220);
 		green = 255 - (int)(12 * (1 - t) * (1 - t) * t * 130);
@@ -50,7 +50,7 @@ static void	set_initial_c(t_complex *z, t_complex *c, t_fractal *fractal)
 		c->x = fractal->julia_x;
 		c->y = fractal->julia_y;
 	}
-	else  if (!ft_strcmp(fractal->name, "mandelbrot"))
+	else if (!ft_strcmp(fractal->name, "mandelbrot"))
 	{
 		c->x = z->x;
 		c->y = z->y;
@@ -64,8 +64,8 @@ static void	handle_pixel(int x, int y, t_fractal *fractal)
 	int			i;
 	int			color;
 
-    z.x = (map(x, -2, 2, WIDTH) * fractal->zoom) + fractal->shift_x;
-    z.y = (map(y, 2, -2, HEIGHT) * fractal->zoom) + fractal->shift_y;
+	z.x = (map(x, -2, 2, WIDTH) * fractal->zoom) + fractal->shift_x;
+	z.y = (map(y, 2, -2, HEIGHT) * fractal->zoom) + fractal->shift_y;
 	set_initial_c(&z, &c, fractal);
 	i = 0;
 	while (i < fractal->iterations_definition)
